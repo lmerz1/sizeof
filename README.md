353f7ede5bebd22e2277424fb403f0a42649dbd5
Lionel Merz <l.merz@tum.de>
2023-07-06 10:44:49 +0200

    Fixed output formatting, added option to choose between 1000 and 1024 for suffix
    (previous todo was: result was different than intended both for files as well as folders that are empty)
    
    Also reworded the "How to get" section to suggest adding the executable file to one's path instead of copying the function into the .*shrc file, and changed the script accordingly

--- README.md
+++ README.md
@@ -25,23 +25,29 @@ $ si $HOME/Pictures
 
 Notably, using si without arguments will default to calculating the size of the current directory.
 
-Also, I haven't bothered to test, but the decimal character from the last example is probably locale-dependent (whatever your implementation of the internally used `du` does)
+Also, I haven't bothered to test, but the decimal character from the last example is probably locale-dependent (whatever your implementation of the internally used `du` does is used)
 
 ## How to get
-Paste the code block in code.sh into your .\{ba,z\}shrc* file.
-
-*May work for more shells, but this has not been tested. 
-In particular, my current use of the `function` keyword is not POSIX-compliant.
-
-# Todos
-
-Formatting is off both for files as well as folders that are empty:
+Add sizeof.sh to your path (e.g. `export PATH="$HOME/scripts:$PATH"` as an executable, and an alias like the following
 
 ```
-$ mkdir tmpdir
-$ touch tmpfile
-$ si tmpdir 
-0 BB
-$ si tmpfile
-0  B
+# sizeof lies in $HOME/scripts
+alias si="sizeof.sh"
+alias sof="sizeof.sh"
+alias sizeof="sizeof.sh"
+```
+
+into your .\{ba,z\}shrc* file.
+
+\*May work for more shells, but this has not been tested. 
+In particular, my current use of the `function` keyword is not POSIX-compliant.
+
+## Settings/Preferences
+
+You can set the "unit" suffixes to be multiples of 1000 or 1024 (Kilo/Mega/Giga/etc. vs. Kibi/Mebi/Gibi/etc.) by changing the first variable's value or passing an optional argument.
+```
+$ si some-dir
+93 MB  # uses default of 1000
+$ si some-dir 1024
+88 MB
 ```
--- sizeof.sh
+++ sizeof.sh
@@ -1,9 +1,17 @@
-# no shebang as this is not meant to be kept in/executed from a separate file,
-# used most easily by pasting the entire function into your .*shrc file
+#!/bin/zsh
 
 function sizeof()
 {
-    powersof=1000 # Regular person's default. Linux extremists might want to change this to 1024. ;)
+    if [ -z "$2" ]; then
+        powersof=1000 # Regular person's default. Linux extremists might want to change this to 1024. ;)
+    else
+        if [ "$2" != 1024 ] && [ "$2" != 1000 ] ; then
+            echo "Change the code if you would like to use a \"base\" different from 1000 or 1024."
+            exit 2
+        fi
+        powersof="$2" # now is 1024 (or 1000)
+    fi
+
 
     passed_name="$1"
 
@@ -14,26 +22,47 @@ function sizeof()
     if   [ -d "$passed_name" ]; then # is a directory or we default to current pwd
         if [ "$powersof" -eq 1000 ]; then
             # du --si -s is equivalent to -d 0:
-            du --si -s "$passed_name"|awk '{ print substr($1,0,(length($1)-1)), substr($1,length($1),1) "B" }'
+            dirsizestring=$(du --si -s "$passed_name")
         else # 1024
-            du -sh "$passed_name"|awk '{ print substr($1,0,(length($1)-1)), substr($1,length($1),1) "B" }'
+            dirsizestring=$(du -sh "$passed_name")
         fi
+
+        dirsize=$(echo "$dirsizestring"|awk '{ print substr($1,0,(length($1)-1)) } ')
+        if [ "$dirsize" = "0" ]; then
+            # use different formatting than default if the directory is empty
+            echo "0 B"
+        else
+            echo "$dirsizestring"|awk '{ print substr($1,0,(length($1)-1)), substr($1,length($1),1) "B" }'
+        fi
+
     elif [ -f "$passed_name" ]; then # is a file
         filesize=$(echo $(wc -c < "$passed_name"))
+
         i=0
-        s=" kMGTPEZY" # lowercase k to imitate behavior of `du` for the sake of consistency
+        if [ "$powersof" -eq 1000 ]; then
+            s=" kMGTPEZY" # lowercase k in this case to imitate behavior of `du` for the sake of consistency
+        else
+            s=" KMGTPEZY" # 1024 used
+        fi
+        
         while [ $filesize -gt "$powersof" ]; do
             i=$((i+1))
             filesize=$((filesize/powersof))
         done
-        echo "$filesize ${s:$i:1}B"
+
+        if [ "$filesize" = "0" ]; then
+            # use different formatting than default if the file is empty
+            echo "0 B"
+        else
+            echo "$filesize ${s:$i:1}B"
+        fi
     else echo "\"$passed_name\" is not valid";
-        #exit 1  ## not exiting the (main) shell session here due to this function being defined within .*shrc files
+        exit 1  ## not exiting the (main) shell session here due to this function being defined within .*shrc files
     fi
 }
 
-alias si="sizeof"
-alias sof="sizeof"
+# alias si="sizeof.sh"
+# alias sof="sizeof.sh"
 
 
-sizeof "$1"
+sizeof "$@"
